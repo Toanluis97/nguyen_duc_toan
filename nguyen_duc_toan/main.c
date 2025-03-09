3f55e8b8cef52f1166b78914dd3d59b04b824a07
Toanluis97 <ntoan7375@gmail.com>
2025-03-09 09:48:12 +0700

    Them mot ham cong hai so a va b

--- nguyen_duc_toan/main.c
+++ nguyen_duc_toan/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 void main()
 {
+	int x = sum(1, 2);
+	printf("%d \n", x);
 	printf("Hello World!");
 }
---
+++ nguyen_duc_toan/math.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+Chuc nang: tao mot ham cong tong cua hai so nguyen a va b
+input
+ int a: so nguyen thu nhat
+ int b: so nguyen thu hai
+ Output
+ int: ket qua
+*/
+int sum(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return sum;
+}
