834849e2427db6f5db6427c3cc7ab5833744f076
Toanluis97 <ntoan7375@gmail.com>
2025-03-09 11:38:07 +0700

    Update code

--- nguyen_duc_toan/main.c
+++ nguyen_duc_toan/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 void main()
 {
-	int arr[5] = { 1,5, 10, 35,56 };
-	int max;
+	int arr[5] = { 1,5, 78, 35,56 };
+	int max = arr[0];
 	for (int i = 0; i < 5; i++)
 	{
-		max = arr[i];
 		if (arr[i] > max)
 			max = arr[i];
 	}
