91c342a9d12cfcdb03a020a4584ef873a72ab574
Toanluis97 <ntoan7375@gmail.com>
2025-03-09 13:26:43 +0700

    Update file main

--- nguyen_duc_toan/main.c
+++ nguyen_duc_toan/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/*
+Tim gia tri lon nhat trong mang
+Input: 
+mang
+
+*/
 void main()
 {
-	int arr[5] = { 1,5, 78, 35,56 };
+	int arr[5] = { 1,100, 78, 35,56 };
 	int max = arr[0];
 	for (int i = 0; i < 5; i++)
 	{
