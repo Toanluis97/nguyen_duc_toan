b06745c18b65cdce01f2d442e1acd31ef15dbd52
Toanluis97 <ntoan7375@gmail.com>
2025-03-16 09:59:29 +0700

    In ra man hinh ky tu viet hoa

--- nguyen_duc_toan/main.c
+++ nguyen_duc_toan/main.c
@@ -3,12 +3,15 @@
 void main()
 {
 	//khai bao chuoi. In ra so luong ky tu trong chuoi
-	char str[] = "hello";
-	int num = 0;
-	for (int i = 0; str[i]!= '\0';i++)
-		num++;
-	num++;
-	printf("So luong ky tu trong chuoi la:%d", num);
+	char str[] = "abcXYZT";
+	//In ra ky tu viet hoa trong chuoi
+	int x = 0;
+	for (int i=0; str[i]!=0;i++)
+	if ((str[i] >= 65) && (str[i] <= 90))
+	{
+		x = str[i];
+		printf("%c", x);
+	}
 }
 
 	
