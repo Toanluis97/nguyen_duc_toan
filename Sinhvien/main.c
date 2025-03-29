62b618c942b3b9989e4324693f14d8aad61f0ddd
Toanluis97 <ntoan7375@gmail.com>
2025-03-29 20:42:18 +0700

    Update bai tap phan so

---
+++ Sinhvien/main.c
@@ -0,0 +1,14 @@
+﻿//1. scan từ bàn phím n.sao đó nhập n học sinh
+//2. viết hàm in ra màn hình danh sách n học sinh này
+//3. viết hàm in ra học sinh có điểm trung bình cao nhất
+//4. viết hàm sắp xếp danh sách học sinh theo điểm trung bình từ cao đến thấp
+//học sinh :
+//tên
+//tuổi
+//gioi tinh
+//điểm toán
+//điểm văn
+//diem trung binh
+//xep loai
+//
+//điểm trung bình = (điểm toán + điểm văn) / 2
